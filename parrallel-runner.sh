90798c675b8344a8a5909c916cce9e1e21c8b871
Amin Moradi <aminmoradi@ip-172-31-34-47.eu-central-1.compute.internal>
2021-07-05 07:55:25 +0000

    Runner debug run-03

--- parrallel-runner.sh
+++ parrallel-runner.sh
@@ -4,5 +4,12 @@ GROUP=$1
 let "run_0 = (2 * $GROUP)"
 let "run_1 = (2 * $GROUP) + 1"
 bash $HOME/fed-benchmarks/aggrigator.sh "$run_0" &
-bash $HOME/fed-benchmarks/aggrigator.sh "$run_0" &
+pids[1]=$!
 
+bash $HOME/fed-benchmarks/aggrigator.sh "$run_0" &
+pids[2]=$!
+
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
